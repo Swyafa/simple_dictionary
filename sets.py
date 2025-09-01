3e19e3065d45107d9d55e9232caffd40158a0ee8
Sami <74431946+Swyafa@users.noreply.github.com>
2025-09-01 17:50:23 +0100

    Set example for fruits

---
+++ sets.py
@@ -0,0 +1,14 @@
+# Write code below 💖
+
+fruits = {'oranges','apples','banana','strawberry'}
+friend_fruit = {'kiwi','apples','strawberry','blueberry'}
+
+#UNION
+union = fruits.union(friend_fruit)
+
+#INTERSECTION
+intersection = fruits.intersection(friend_fruit)
+
+print(union)
+print()
+print(intersection)
