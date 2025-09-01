5c4797abe9e5e9cc28287c39bc61a4709e389e28
Sami <74431946+Swyafa@users.noreply.github.com>
2025-09-01 17:36:02 +0100

    added dictionary for artefact from
    https://www.metmuseum.org/art/collection/search/442986

---
+++ test.py
@@ -0,0 +1,13 @@
+Sadequain = {
+    'Title':'Seascape with Three Boats',
+    'Date':'20th century',
+    'Geography':'Made in Pakistan',
+    'Medium':'Oil on wood',
+    'Dimensions':'H.77.5 cm, W. 81.3 cm',
+    'Object Number':'2016.12'
+    }
+print('Dictionary: ', Sadequain)
+print() 
+print("Keys: ", Sadequain.keys())
+print()
+print("Values: ", Sadequain.values())
